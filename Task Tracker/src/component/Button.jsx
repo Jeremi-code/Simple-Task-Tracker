8121cc94d5a1b1641c3f9b1491e93af0354ae137
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-07-22 11:42:05 -0700

    add default prop and proptype to button component

--- Task Tracker/src/component/Button.jsx
+++ Task Tracker/src/component/Button.jsx
@@ -1,9 +1,16 @@
+import PropTypes from 'prop-types'
 
 const Button = (props)=>{
     return(
         <div>
-            <button className="btn">{props.title}</button>
+            <button className="btn" style={{backgroundColor:props.color}}>{props.title}</button>
         </div>
     )
+}
+Button.defaultProps = {
+    color:'green',
+}
+Button.propTypes = {
+    title: PropTypes.string.isRequired,
 }
 export default Button
