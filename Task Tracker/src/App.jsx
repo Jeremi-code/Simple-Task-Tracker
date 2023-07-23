398d0025afd90268c318fdcaef95cce869a14299
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-07-23 04:14:48 -0700

    update task state from form input

--- Task Tracker/src/App.jsx
+++ Task Tracker/src/App.jsx
@@ -36,7 +36,9 @@ export default function App() {
   }
   
   const addTask = (task) => {
-    console.log(task)
+    const id = Math.floor(Math.random()*10000)+1
+    const newTask = {id,...task}
+    setTasks([...tasks,newTask])
   }
 
   return (
