5f4e1fa82e78acedac4e18956c77037414a5a0c6
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-07-23 04:33:14 -0700

    create state for the add button in the header to make the form shown and not shown

--- Task Tracker/src/App.jsx
+++ Task Tracker/src/App.jsx
@@ -6,6 +6,7 @@ import Tasks from './component/Tasks'
 import AddTask from './component/AddTask'
 
 export default function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -43,8 +44,8 @@ export default function App() {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header onAdd={() => setShowAddTask(!showAddTask)}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : "No Tasks To Show"
       }
       </div>
