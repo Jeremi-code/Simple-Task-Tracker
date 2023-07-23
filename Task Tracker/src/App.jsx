2b2001d8c3fdc9caeaf208c0a965966252ee91f6
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-07-23 13:26:18 -0700

    Delete tasks from this component

--- Task Tracker/src/App.jsx
+++ Task Tracker/src/App.jsx
@@ -8,24 +8,7 @@ import AddTask from './component/AddTask'
 export default function App() {
   const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctors Appointment',
-      day: 'Feb 5th at 2:30pm',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Meeting at School',
-      day: 'Feb 6th at 1:30pm',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Food Shopping',
-      day: 'Feb 5th at 2:30pm',
-      reminder: false,
-    },
+    
   ])
 
   const deleteTask = (id) => {
